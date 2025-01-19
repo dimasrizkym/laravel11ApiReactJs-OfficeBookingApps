fc29f994ace59fbc3ff10ab2cff4d83b173656a0
Dimas Rizky <dimasrizkym3@gmail.com>
2025-01-19 12:47:05 +0700

    feat: implement mass assignment and ORM relationships for models

--- backend/app/Models/OfficeSpacePhoto.php
+++ backend/app/Models/OfficeSpacePhoto.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class OfficeSpacePhoto extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'photo',
+        'office_space_id',
+    ];
+
 }
